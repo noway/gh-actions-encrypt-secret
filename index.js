9a88076a46f65f65cfe0a4d04a2fc405167c069c
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-19 00:19:09 +1200

    use stdin

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const sodium = require('tweetsodium');
+
 const key = process.argv[2];
-const value = process.argv[3];
+const value = fs.readFileSync(process.stdin.fd, 'utf-8');
+
 const messageBytes = Buffer.from(value);
 const keyBytes = Buffer.from(key, 'base64');
 const encryptedBytes = sodium.seal(messageBytes, keyBytes);
