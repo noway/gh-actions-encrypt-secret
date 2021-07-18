0cf234a217c5438ed6aba068d65d8f35bca878c5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-18 18:07:12 +1200

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const sodium = require('tweetsodium');
+const key = process.argv[2];
+const value = process.argv[3]
+const messageBytes = Buffer.from(value);
+const keyBytes = Buffer.from(key, 'base64');
+const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+const encrypted = Buffer.from(encryptedBytes).toString('base64');
+console.log(encrypted);
