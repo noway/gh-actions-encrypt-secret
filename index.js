695274b63dc48a64c213eea26842af90d4009dd5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-18 18:19:13 +1200

    fix bin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 const sodium = require('tweetsodium');
 const key = process.argv[2];
-const value = process.argv[3]
+const value = process.argv[3];
 const messageBytes = Buffer.from(value);
 const keyBytes = Buffer.from(key, 'base64');
 const encryptedBytes = sodium.seal(messageBytes, keyBytes);
